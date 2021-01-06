fc96907e0c29a693c359a0ac5f9ba873d17bd12d
Antonio Manzanares <anclist@gmail.com>
2021-01-06 14:08:47 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,10 @@
+# puts "What is your first name?"
+# first_name = gets.chomp
+# puts "What is your last name?"
+# last_name = gets.chomp
+# full_name = "#{first_name} #{last_name}"
+# puts "Hello #{full_name}"
+# puts "Your name in reverse is #{full_name.reverse!.downcase!}"
+# puts "Also your name has #{first_name.length + last_name.length} characters"
+
+
